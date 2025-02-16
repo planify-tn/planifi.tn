e5015b04319fb0d5d274024c30242f4b816bf7d5
Aymen KHEDHRIYA <aymen.khedhriya@gmail.com>
2025-02-16 17:24:40 +0100

    fix: logo text

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -34,7 +34,7 @@ function Nav() {
         <nav className='sticky top-0 flex flex-col items-center justify-between w-full gap-4 p-4 lg:gap-0 lg:flex-row bg-background min-h-[var(--navbar-height)] z-50'>
             <Link href="/" className='flex items-center gap-2'>
                 <img src="./logo-planifi.svg" alt="logo" width={30} />
-                <span className='uppercase font-semibold'>Planifi</span>
+                <span className='uppercase font-semibold text-white'>Planifi</span>
             </Link>
             <div className='flex gap-8'>
                 {routes.map((route, index) => (
